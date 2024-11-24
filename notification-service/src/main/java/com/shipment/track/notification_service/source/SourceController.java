6d70376424f0b408e0d19f77b7da660b1bc2b0df
sourabh2099 <sourabhchowdhury82@gmail.com>
2024-11-24 10:26:37 +0530

    email sender addition

--- notification-service/src/main/java/com/shipment/track/notification_service/source/SourceController.java
+++ notification-service/src/main/java/com/shipment/track/notification_service/source/SourceController.java
@@ -2,12 +2,13 @@ package com.shipment.track.notification_service.source;
 
 import com.shipment.track.notification_service.dto.NotificationMessage;
 import com.shipment.track.notification_service.service.OrchestratorService;
-import com.shipment.track.notification_service.validators.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,7 +20,8 @@ public class SourceController {
     public SourceController(OrchestratorService orchestratorService) {
         this.orchestratorService = orchestratorService;
     }
-
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     @PostMapping("/send-notification")
     public ResponseEntity<?> processSendNotificationRequest(@RequestBody NotificationMessage notificationMessage) {
@@ -41,6 +43,18 @@ public class SourceController {
     @GetMapping("/hello")
     public ResponseEntity<?> testController() {
         return ResponseEntity.ok("Got Request !!");
+    }
+
+    @PostMapping("/send-email")
+    public ResponseEntity<?> sendEmail() {
+        LOG.info("sending email ");
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("chowdhurysourabh58@gmail.com");
+        message.setTo("sourabhchowdhury82@gmail.com");
+        message.setSubject("Shipment status update");
+        message.setText("hello world");
+        javaMailSender.send(message);
+        return ResponseEntity.accepted().body("Sent Mail");
     }
 
 }
