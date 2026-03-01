bdf1691dbf0821a9c4b704130f16c759dabc0989
sourabh2099 <sourabhchowdhury82@gmail.com>
2026-03-01 16:52:02 +0530

    added dockerization for notification service

--- location-service/src/main/java/com/shipment/track/location/service/config/AppConfig.java
+++ location-service/src/main/java/com/shipment/track/location/service/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.shipment.track.location.service.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
@@ -8,10 +11,17 @@ import org.springframework.data.mongodb.core.ReactiveMongoClientFactoryBean;
 @Configuration
 public class AppConfig extends AbstractReactiveMongoConfiguration {
 
+    @Value("${locationService.data.mongodb.uri}")
+    private String mongoDbUri;
+
+    private static final Logger LOG = LoggerFactory.getLogger(AppConfig.class);
+
     @Bean
     public ReactiveMongoClientFactoryBean mongoConfig() {
+        LOG.info("Mongo DB URI {}", mongoDbUri);
+
         ReactiveMongoClientFactoryBean bean = new ReactiveMongoClientFactoryBean();
-        bean.setConnectionString("mongodb://localhost:27017/" + getDatabaseName());
+        bean.setConnectionString(mongoDbUri + getDatabaseName());
 //        bean.setMongoClientSettings(MongoClientSettings.builder()
 //                .addCommandListener()
 //                        .credential(MongoCredential.createCredential())
